f284c10637caf0eab4c0b19db1bd518e4cdbd205
Alex Martinez <alexandr3martin3z@gmail.com>
2019-10-10 04:23:15 +0000

    Automatically backed up by Learn

--- grandma.rb
+++ grandma.rb
@@ -5,7 +5,7 @@ def speak_to_grandma(string)
     return "I LOVE YOU TOO PUMPKIN!"
   elsif string == "Hi Nana, how are you?"
     return "HUH?! SPEAK UP, SONNY!"
-  else string == string.upcase
+  else string == "Hi!"
     return "NO, NOT SINCE 1938!"
   end
 
